efe57ebdd25526d2247b55c84b3dcce119920bec
TintinDeBrest <45122828+TintinDeBrest@users.noreply.github.com>
2026-07-28 23:44:28 +0200

    Enhance compute_embeddings with type hints and docstring
    Updated compute_embeddings function to include type hints and improved docstring.

--- explorer/embeddings.py
+++ explorer/embeddings.py
@@ -1,9 +1,12 @@
 # embeddings
+# PhL 28jul26
 
 from sentence_transformers import SentenceTransformer
+from explorer.config import MODEL_NAME
+from typing import Sequence
+import numpy as np
 
-MODEL_NAME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-_model = None
+_model = None # used cached model if present
 
 def load_model():
     global _model
@@ -11,6 +14,24 @@ def load_model():
         _model = SentenceTransformer(MODEL_NAME)
     return _model
 
-def compute_embeddings(words):
+
+def compute_embeddings(words: Sequence[str]) -> np.ndarray:
+    """
+    Compute embeddings for a list of words or sentences.
+    Parameters
+    ----------
+    words : list[str]
+    Returns
+    -------
+    numpy.ndarray
+    """
+    if isinstance(words, str): # Allow single word "chat" or ["chat"]
+        words = [words]
+                
+    print(f"Loading {MODEL_NAME}")
     model = load_model()
-    return model.encode(words)
+    
+    return model.encode(
+    words,
+    convert_to_numpy=True
+)
